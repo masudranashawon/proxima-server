23f6a66f1ddc43b26ca984e4aac49a64ad9ebc20
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-02-25 22:21:59 +0600

    JWT added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 //Login
 const loginUser = async (req, res) => {
@@ -12,7 +17,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
-    res.status(200).json({ user });
+    //Create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
